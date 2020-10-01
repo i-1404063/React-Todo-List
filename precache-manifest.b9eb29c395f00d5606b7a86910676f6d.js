2b665820d17cd8af2ffe7ce5193f355d1e4a08b8
unknown <imonhasans33@gmail.com>
2020-10-01 23:25:24 +0600

    Updates

---
+++ precache-manifest.b9eb29c395f00d5606b7a86910676f6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005757b8acba24283352d764c3dd5f15",
+    "url": "/React-Todo-List/index.html"
+  },
+  {
+    "revision": "93b38882bf6c7404c10c",
+    "url": "/React-Todo-List/static/css/main.02688281.chunk.css"
+  },
+  {
+    "revision": "92b772da6c76fd11349f",
+    "url": "/React-Todo-List/static/js/2.a7b2d87b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Todo-List/static/js/2.a7b2d87b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93b38882bf6c7404c10c",
+    "url": "/React-Todo-List/static/js/main.e20fa96c.chunk.js"
+  },
+  {
+    "revision": "a774db6da9fdb48f2769",
+    "url": "/React-Todo-List/static/js/runtime-main.23519940.js"
+  }
+]);
